39d4a8734a715033f24bc848d28d9617b74616d6
Douglas Crews <douglas.crews@partech.com>
2023-09-14 08:38:50 -0700

    Code cleanup on bashrc.aws.sh

--- bashrc.aws.sh
+++ bashrc.aws.sh
@@ -7,18 +7,18 @@ alias aws_all_list='awsls --profiles dougcrews --attributes tags,cidr_block aws_
 aws_assume_role() {
 	# help
 	[[ "${1}" == "--help" ]] || [[ "${1}" == "-h" ]] || [[ -z "${1}" ]] && { \
-		echo -e "${FUNCNAME}()\n\t \"role-ARN\" ARN for the Role to assume\n\t [profile] AWS profile to assume"; \
+		echo -e "${FUNCNAME}()\n\t role_arn ARN for the Role to assume\n\t [profile] AWS profile to assume (default \"service\")"; \
 		return 0; \
 	}
 	local role_arn="$1";
 	local aws_profile="${2:-service}"
 	[[ -z "${role_arn}" ]] && { echo 'Role ARN is required'; return 0; }
 	echo "Assuming role ${role_arn} in profile ${aws_profile}"
-	echo export ROLE_CREDENTIALS=$(aws sts assume-role --profile ${aws_profile} --role-arn "${role_arn}" --role-session-name AWSCLI-Session --output json)
 	export ROLE_CREDENTIALS=$(aws sts assume-role --profile ${aws_profile} --role-arn "${role_arn}" --role-session-name AWSCLI-Session --output json)
 	export AWS_ACCESS_KEY_ID=$(echo $ROLE_CREDENTIALS | jq .Credentials.AccessKeyId | sed 's/"//g')
 	export AWS_SECRET_ACCESS_KEY=$(echo $ROLE_CREDENTIALS | jq .Credentials.SecretAccessKey | sed 's/"//g')
 	export AWS_SESSION_TOKEN=$(echo $ROLE_CREDENTIALS | jq .Credentials.SessionToken | sed 's/"//g')
+	( [[ -n "${AWS_ACCESS_KEY_ID}" ]] && [[ -n "${AWS_SECRET_ACCESS_KEY}" ]] && [[ -n "${AWS_SESSION_TOKEN}" ]] && echo "Success!" ) || echo "Failed."
 }
 
 aws_sso_account ()
